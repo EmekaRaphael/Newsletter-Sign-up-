3613a6957692f967b4dab05164192403f1f1430c
EmekaRaphael <emekaraphael13@gmail.com>
2023-10-26 19:27:19 +0100

    few changes made

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -60,6 +60,6 @@ app.post("/failure", (req, res) => {
     res.redirect("/");
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${port}!`);
 });
